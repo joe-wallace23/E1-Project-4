c2f7a56d2507ad0e4b6524edcb922821619e4ff5
Joe Wallace <joewallace@Joes-Air-010013004101.MissouriState.edu>
2021-03-14 18:12:52 -0500

    gitignore, Procfile

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get("/success", function(req,res){
   res.render("success")
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server has started Successfully")
 });
